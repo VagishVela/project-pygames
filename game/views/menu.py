d93b24f9bf8afebf4c58274965b3cd6301a47bd5
Vagish Vela <git@vagish.com>
2023-04-05 15:23:24 +0100

    fix update state on menu

--- game/views/menu.py
+++ game/views/menu.py
@@ -1,8 +1,6 @@
 """ Implements the Menu view """
 
-from game.helper import Text
-
-# from game.helper import Button
+from game.helper import Text, Button
 from views import View
 
 
@@ -11,25 +9,25 @@ class Menu(View):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        # self.options = ["Option 1", "Option 2", "Option 3"]
-        # self.buttons = [Button(
-        #     self.width / 2,
-        #     self.height / 2 + i * 60,
-        #     100,
-        #     50,
-        #     option,
-        #     onclick=lambda: print("button pressed!", option)
-        # ) for i, option in enumerate(self.options)]
-        # print(self.buttons)
+        self.options = ["Option 1", "Option 2", "Option 3"]
+        self.buttons = [
+            Button(
+                self.width / 2,
+                self.height / 2 + i * 60,
+                100,
+                50,
+                option,
+                onclick=lambda: print("button pressed!", option),
+            )
+            for i, option in enumerate(self.options)
+        ]
+        print(self.buttons)
 
-    # def on_update(self):
-    #     for b in self.buttons:
-    #         b.update()
+    def on_update(self):
+        for b in self.buttons:
+            b.update()
 
     def on_draw(self):
-        # for b in self.buttons:
-        #     b.blit_into(self.screen)
-
         self.screen.fill("black")
         Text(
             "Menu",
@@ -39,14 +37,9 @@ class Menu(View):
             200,
             "red",
         ).blit_into(self.screen)
-        Text(
-            "Click to return!",
-            self.font,
-            self.width / 2,
-            self.height / 2 + 50,
-            50,
-            "white",
-        ).blit_into(self.screen)
+
+        for b in self.buttons:
+            b.blit_into(self.screen)
 
     def on_click(self):
         # import here to avoid circular imports
