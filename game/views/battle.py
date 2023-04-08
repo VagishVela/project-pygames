6836c59f468bdad219b0e6e68c502c3d7c0a87f8
ShahnawazHussain28 <shahnawazhussain2802@gmail.com>
2023-04-08 14:47:29 +0530

    Added projectile

--- game/views/battle.py
+++ game/views/battle.py
@@ -1,9 +1,10 @@
 """ Implements the Battle view """
 
 import pygame
-from game.helper import Text, Button
-
+from pygame import Surface
+from pygame.sprite import Sprite
 from views import View
+import math
 
 
 class Battle(View):
@@ -31,9 +32,11 @@ class Battle(View):
             "health": 70,
         }
         self.projectiles = []
+        self.player_attack()
 
     def on_update(self):
         for event in pygame.event.get():
+            print("EVENT")
             if event.type == pygame.KEYDOWN:
                 if self.my_turn:
                     if event.key == pygame.K_a:
@@ -43,16 +46,37 @@ class Battle(View):
                         self.player_dodge()
         
 
-    def player_attck():
+    def player_attack(self):
         # play some Attack animation
-
-        pass
+        projectile_direction = (
+            self.enemy_pos[0] - self.player_pos[0],
+            self.enemy_pos[1] - self.player_pos[1],
+        )
+        self.projectiles.append(Projectile(self.player_pos, projectile_direction))
 
     def on_draw(self):
         self.screen.fill("#333333")
         self.screen.blit(self.player_image, (self.player_pos[0], self.player_pos[1]))
         self.screen.blit(self.enemy_image, (self.enemy_pos[0], self.enemy_pos[1]))
+        for projectile in self.projectiles:
+            projectile.update()
+            projectile.draw(self.screen)
 
     def on_click(self):
         pass
 
+class Projectile(Sprite):
+    def __init__(self, pos: tuple[int, int] | list[int, int], direction: tuple[int, int]) -> None:
+        self.x, self.y = pos
+        self.direction = direction
+        self.speed = 5
+        self.image = pygame.transform.rotozoom(
+            pygame.image.load("assets/fireball_1.png"), 
+            -math.degrees(math.atan2(self.direction[1], self.direction[0])),
+            1.5
+        )
+    def update(self):
+        self.x += self.direction[0] * self.speed * 0.001
+        self.y += self.direction[1] * self.speed * 0.001
+    def draw(self, screen: Surface):
+        screen.blit(self.image, (self.x, self.y))
