5faacf145ea0649ed0f84e6e9cf3367110839089
Vagish Vela <git@vagish.com>
2023-04-14 16:25:44 -0500

    move the attack to player/enemy

--- game/entities/enemy.py
+++ game/entities/enemy.py
@@ -26,6 +26,13 @@ class Enemy(Sprite):
         self.visible = True
         self.name = "Alien"
 
+        self.attacks = [
+            {"name": "Quick Attack", "power": 10},
+            {"name": "Power Attack", "power": 20},
+            {"name": "Super Attack", "power": 30},
+            {"name": "Mega Attack", "power": 40},
+        ]
+
     def take_damage(self, p_ability):
         """Take damage from the player"""
         self.abilities["health"] -= (
